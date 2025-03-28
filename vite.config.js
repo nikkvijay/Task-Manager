70b24739129b5c922602c80feead749c979659a9
nikkvijay <jatalenikhileshwar@gmail.com>
2025-03-29 03:57:26 +0530

    delpoy fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Task-manager",
+  base: "/Task-Manager/", // Add this line
 });
